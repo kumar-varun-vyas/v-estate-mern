c59ba10ed5da101e19edbbf2376d07ca24832e4e
kumar-varun-vyas <kumarvarunvyas55@gmail.com>
2024-04-18 00:01:39 +0530

    connect to db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
 const PORT = 3000
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("mongodb connected successfully")
+}).catch(err => {
+    console.log("mongodb connection err -", err)
+})
 
 
 app.listen(PORT, () => {
