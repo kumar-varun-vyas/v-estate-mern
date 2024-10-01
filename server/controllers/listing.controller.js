7d7ac73ef204970c20dc5e28ca76ac5266e121e4
kumar-varun-vyas <kumarvarunvyas55@gmail.com>
2024-10-01 21:55:24 +0530

    search api created

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -58,7 +58,7 @@ const updateListing = async (req, res, next) => {
 const getListing = async (req, res, next) => {
     try {
         const listing = await Listing.findById(req.params.id);
-        console.log("listing-----", listing)
+        // console.log("listing-----", listing)
         if (!listing) {
             return next(customError(404, 'Listing not found!'))
         }
@@ -83,9 +83,61 @@ const getListing = async (req, res, next) => {
     }
 }
 
+const getListings = async (req, res, next) => {
+    try {
+
+        let limit = req.query.limit ? parseInt(req.query.limit) : 9;
+        console.log("query-----------", req.query)
+        let startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer;
+        if (!offer) {
+            offer = { $in: [false, true] }
+        }
+        let furnished = req.query.furnished;
+        if (!furnished) {
+            furnished = { $in: [false, true] }
+        }
+        let parking = req.query.parking;
+        if (!parking) {
+            parking = { $in: [false, true] }
+        }
+        let pet = req.query.pet;
+        if (!pet) {
+            pet = { $in: [false, true] }
+        }
+        let type = req.query.type;
+        if (!type) {
+            type = { $in: ['sale', 'rent'] }
+        }
+        let searchTerm = req.query.searchTerm || ''
+        let sort = req.query.sort || 'createdAt'
+        let order = req.query.order || 'desc'
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i' },
+            offer,
+            furnished,
+            parking,
+            type,
+
+        }).sort(
+            { [sort]: order }
+        ).limit(limit).skip(startIndex)
+
+        res.status(200).json({
+            success: true,
+            message: "",
+            data: listings
+        })
+
+    } catch (err) {
+        next(err)
+    }
+}
+
 module.exports = {
     createListing,
     deleteListing,
     updateListing,
-    getListing
+    getListing,
+    getListings
 }
